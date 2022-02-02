5617b98fee4d0458bebefef02c3fbc84921ac8a9
yerinko <koyerin956@gmail.com>
2022-02-02 23:42:09 +0900

    03. boolean

---
+++ boolean.ts
@@ -0,0 +1,9 @@
+let isDone: boolean = false;
+
+isDone = true;
+
+console.log(typeof isDone); // 'boolean'
+
+let isOk: boolean = true;
+
+// let isNotOk: boolean = new Boolean(true); typescript에서는 이러한 방법보다는 위에  literal 방법으로 작성한다.
